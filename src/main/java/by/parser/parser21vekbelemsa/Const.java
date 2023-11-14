dffbe5d6574af0b30fe7bf6df8b2ef1f2412cbcc
Vitaly Gukov <vgukov@outlook.com>
2023-11-14 17:39:57 +0300

    Fix some problem

--- src/main/java/by/parser/parser21vekbelemsa/Const.java
+++ src/main/java/by/parser/parser21vekbelemsa/Const.java
@@ -23,7 +23,7 @@ public class Const {
 
             *********************************Настройки************************************************
 
-            #userAgent = Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36
+            #userAgent = Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.2151.58
 
             ******************************************************************************************
 
---
+++ src/main/java/by/parser/parser21vekbelemsa/Main.java
@@ -0,0 +1,7 @@
+package by.parser.parser21vekbelemsa;
+
+public class Main {
+    public static void main(String[] args) {
+        StartApplication.main(args);
+    }
+}
